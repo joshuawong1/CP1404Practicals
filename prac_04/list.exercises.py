5f8dbf52dd493cea8a5cac59d45a4a588f5315de
joshuawong1 <joshuawong0812@gmail.com>
2019-09-01 22:13:25 +1000

    - Add informative print messages

--- prac_04/list.exercises.py
+++ prac_04/list.exercises.py
@@ -1,4 +1,9 @@
 numbers = []
 for i in range(5):
-    new_number = int(input('Enter a number between 1 and 5: '))
+    number = int(input('Enter a number between 1 and 5: '))
     numbers.append(number)
+print('The first number is', numbers[0])
+print("The last number is", numbers[-1])
+print("The smallest number is", min(numbers))
+print("The largest number is", max(numbers))
+print("The average of the numbers is", sum(numbers) / len(numbers))
