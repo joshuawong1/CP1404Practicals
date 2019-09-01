c5df7f3b7ae54d1047443d7807c8159c3d55dac0
joshuawong1 <joshuawong0812@gmail.com>
2019-09-01 22:24:41 +1000

    - Add def main() function - Add number generator functionality - Number sort functionality

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,21 @@
+def main():
+    number_limit = 6
+    minimum_number = 1
+    maximum_number = 45
+    number_of_quick_picks = int(input("Enter amount of quick picks: "))
+    while number_of_quick_picks < 0:
+        print("Enter a valid number of quick picks: ")
+        number_of_quick_picks = int(input("Enter amount of quick picks: "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(number_limit):
+            number = random.randint(minimum_number, maximum_number)
+            while number in quick_pick:
+                number = random.randint(minimum_number, maximum_number)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
