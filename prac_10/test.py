af2a20ba8352e2decdd26a3014b8fa2cf9c4a598
joshuawong1 <joshua.wong@my.jcu.edu.au>
2019-10-28 22:48:01 +1000

    - Add test.py

--- prac_10/test.py
+++ prac_10/test.py
@@ -19,7 +19,7 @@ def is_long_word(word, length=5):
     False
     >>> is_long_word("supercalifrag")
     True
-    >>> is_long_word("Python", 6)
+    >>> is_long_word("Python")
     True
     """
     return len(word) >= length
@@ -27,7 +27,6 @@ def is_long_word(word, length=5):
 
 def run_tests():
     """Run the tests on the functions."""
-    # assert test with no message - used to see if the function works properly
     assert repeat_string("Python", 1) == "Python"
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
@@ -35,10 +34,6 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
     test_car = Car(fuel=10)
     assert test_car.fuel == 10
 
@@ -46,21 +41,21 @@ def run_tests():
     assert test_car.fuel == 0
 
 
+def phrase_to_sentence(phrase):
+    """
+    >>> phrase_to_sentence('hello')
+    "Hello."
+    >>> phrase_to_sentence('It is an ex parrot.')
+    "It is an ex parrot."
+    >>> phrase_to_sentence('I like pizza')
+    "I like pizza"
+    """
+    sentence = phrase.capitalize()
+    if sentence[-1] != '.':
+        sentence += '.'
+    return sentence
+
+
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
 doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-# 'hello' -> 'Hello.'
-# 'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more you decide (one that is valid!)
-# test this and watch the tests fail
-# then write the body of the function so that the tests pass
